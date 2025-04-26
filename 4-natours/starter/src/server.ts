8154a0979492e17a5f8ea9ccb6565d6b5f80d6b5
chiarapironti-94 <chiarapironti94@gmail.com>
2025-04-26 08:57:20 +0200

    database

--- 4-natours/starter/src/server.ts
+++ 4-natours/starter/src/server.ts
@@ -1,6 +1,22 @@
+import mongoose from 'mongoose';
 import './config';
-
 import app from './app';
+
+const DB = process.env.DATABASE?.replace(
+  '<DB_PASSWORD>',
+  process.env.DATABASE_PASSWORD as string
+) as string;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connection with the DB established!');
+  });
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}...`);
